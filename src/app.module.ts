eedd0fe57932accb5d5b23df2271192e3bcaff12
bervalim <bernardogvalim@gmail.com>
2024-05-28 17:37:24 -0300

    feat/running migration on project

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { MovieModule } from './modules/movie/movie.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PostgresConfigService } from './config/postgres.config.service';
 import { ConfigModule } from '@nestjs/config';
 import { ReservationModule } from './modules/reservation/reservation.module';
 import { GenreModule } from './modules/genre/genre.module';
@@ -15,10 +14,22 @@ import { AuthModule } from './modules/auth/auth.module';
   imports: [
     UsersModule,
     MovieModule,
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
     TypeOrmModule.forRootAsync({
-      useClass: PostgresConfigService,
-      inject: [PostgresConfigService],
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT, 10),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/../migrations/**/*{.ts,.js}'],
+        synchronize: false,
+      }),
     }),
     ReservationModule,
     GenreModule,
